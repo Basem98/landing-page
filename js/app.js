42a112a891f8a81fc938f7bbd150905510e1f57f
Basem98 <basem.mostafa98@hotmail.com>
2020-08-12 19:26:15 +0200

    feat: Build the navigation menu

--- js/app.js
+++ js/app.js
@@ -1,40 +1,54 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
+ *
 */
 
 /**
  * Define Global Variables
- * 
+ *
 */
 
+// The container for all the sections in index.html
+const sections = document.querySelectorAll('section');
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
+ *
 */
-
 
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ *
 */
 
 // build the nav
-
+function buildNavBar() {
+    const docFragment = document.createDocumentFragment();
+    const listOfItems = document.getElementById('navbar__list');
+    sections.forEach((section) => {
+        const listItem = document.createElement('li');
+        const anchor = document.createElement('a');
+        anchor.setAttribute('class', 'menu__link');
+        anchor.setAttribute('href', `#${section.id}`);
+        anchor.textContent = section.dataset.nav;
+        listItem.appendChild(anchor);
+        docFragment.appendChild(listItem);
+    });
+    listOfItems.appendChild(docFragment);
+}
 
 // Add class 'active' to section when near top of viewport
 
@@ -45,10 +59,11 @@
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
-// Build menu 
+document.addEventListener('DOMContentLoaded', buildNavBar);
+// Build menu
 
 // Scroll to section on link click
 
