d07aa92976441ee375e8cf245937883104ea1572
Basem98 <basem.mostafa98@hotmail.com>
2020-08-13 19:39:57 +0200

    feat: Activate each navigation link when its section is active

--- js/app.js
+++ js/app.js
@@ -61,29 +61,35 @@ function buildNavBar() {
 
 function activateSection() {
     const currentSelectedSection = document.querySelector('.your-active-class');
+    const associatedLink = document.querySelector(`a[href='#${currentSelectedSection.id}']`);
     /**
      *  The section's size and position relative to the user's viewport
      */
 
     const boundingClientRect = currentSelectedSection.getBoundingClientRect();
-    /**
-     * Start changing the current active section,
-     * only when the user has already reached the first section
-     * (after scrolling past approx. 900 pixels from top)
-     */
 
     if (window.scrollY > 900) {
-        if (boundingClientRect.top < 0 && boundingClientRect.bottom < 70 && currentSelectedSection.nextElementSibling) {
+        /**
+         * Start changing the current active section,
+         * only when the user has already reached the first section
+         * (after scrolling past approx. 900 pixels from top)
+         */
+        if (boundingClientRect.top < 0 && boundingClientRect.bottom < 70
+            && currentSelectedSection.nextElementSibling) {
             currentSelectedSection.classList.remove('your-active-class');
             currentSelectedSection.nextElementSibling.classList.add('your-active-class');
+            associatedLink.classList.remove('menu__link__active');
+            associatedLink.parentElement.nextElementSibling.firstChild.classList.add('menu__link__active');
         }
-        if (boundingClientRect.top > 70 && boundingClientRect.bottom > 0 && currentSelectedSection.previousElementSibling) {
+        if (boundingClientRect.top > 70 && boundingClientRect.bottom > 0
+            && currentSelectedSection.previousElementSibling) {
             currentSelectedSection.classList.remove('your-active-class');
             currentSelectedSection.previousElementSibling.classList.add('your-active-class');
+            associatedLink.classList.remove('menu__link__active');
+            associatedLink.parentElement.previousElementSibling.firstChild.classList.add('menu__link__active');
         }
     } else {
-        currentSelectedSection.classList.remove('your-active-class');
-        document.getElementById('section1').classList.toggle('your-active-class');
+        document.getElementById('section1').classList.toggle('your-active-class', true);
     }
 }
 
@@ -94,14 +100,18 @@ function activateSection() {
  */
 function scrollToSection(event) {
     event.preventDefault();
-    const selectedSectionId = event.target.getAttribute('href').slice(1);
+    document.querySelector('.menu__link__active')?.classList.remove('menu__link__active');
+    event.target.classList.add('menu__link__active');
+    const selectedSectionId = event.target.getAttribute('href')?.slice(1);
     const selectedSection = document.getElementById(selectedSectionId);
-    const sectionBoundigClientRect = selectedSection.getBoundingClientRect();
-    const scrollingOptions = {
-        top: window.scrollY + sectionBoundigClientRect.top,
-        behavior: 'smooth'
-    };
-    window.scrollTo(scrollingOptions);
+    if (selectedSection) {
+        const sectionBoundigClientRect = selectedSection.getBoundingClientRect();
+        const scrollingOptions = {
+            top: window.scrollY + sectionBoundigClientRect.top,
+            behavior: 'smooth'
+        };
+        window.scrollTo(scrollingOptions);
+    }
 }
 
 /**
@@ -115,6 +125,12 @@ function scrollToSection(event) {
  */
 
 document.addEventListener('DOMContentLoaded', buildNavBar);
+
+/**
+ * When the DOM first loads, activate the section in the viewport after loading,
+ * in case this section isn't the first one
+ */
+
 document.addEventListener('DOMContentLoaded', activateSection);
 
 /**
@@ -129,7 +145,4 @@ document.addEventListener('scroll', activateSection);
  */
 
 listOfItems.addEventListener('click', scrollToSection);
-
-// Set sections as active
-
 
